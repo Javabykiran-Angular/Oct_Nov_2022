760b031165dd71d70b2a0d8fad42de089bffffa6
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-01-31 16:10:11 +0530

    Day05

---
+++ Typescript/Day05/demo1.js
@@ -0,0 +1,48 @@
+//data types
+// number(int,float,long,double,short),string(""/''),boolean(true/flase),
+// any(number,string,boolean)
+//void => it is neutral data type=> it does not point anything
+//miesc. null & undefined => it used as value as well as data type
+var a1 = 10; // forward declaration & defination 
+// console.log("Value of a1 "+a1);
+// console.log('==== Value of a1 '+a1);
+// console.log(`value of a1= ${a1} `);
+// back tic operator
+var str;
+// console.log("Value of str is "+str);
+str = 'Sumit';
+// console.log("Value of str is "+str);
+var myany;
+myany = 2.5;
+// console.log("myany is "+myany);
+myany = 'Sumit';
+// console.log("myany is "+myany);
+myany = true;
+// console.log("myany is "+myany);
+var a;
+// Literal type 
+var b1;
+b1 = 45;
+// console.log("Value of b1 "+b1);
+b1 = 'Raokhande';
+// console.log("Value of b1 "+b1);
+var b2;
+b2 = 'Angular';
+var b3 = 78;
+// type assertion
+// it is used to tell to the compiler to access or perform any functionality of that variable
+// pre-requirest 
+// data type should be any/unknown/object
+var c1;
+//1 Angle bracket
+// ts file is used 
+//2 as syntax
+// in html file & ts file 
+//1 Angle bracket
+c1 = 10;
+var temp = c1;
+// temp.
+console.log(c1);
+//2 as syntax
+var temp1 = c1;
+// temp1.
---
+++ Typescript/Day05/demo2.js
@@ -0,0 +1,29 @@
+//operators
+//Airtmathic => +,-,/,*,%
+//logical => &&,||
+//bitwise => !,~,&,|,^,<<,>>
+//relational=> <,>,==(it checks only value),===(strongly equality it checks value as well as data type )
+//conditional=> <=,>=,!=
+//ternary=> condition?expression1:expression2
+//unary => inc/dec/post/pre ++a,a--
+//assignment => =,+=,-=,*=,/=,%=
+//Control statement
+// if-else,nested if-else,switch,break,continue
+var t1 = 21;
+// if(t1<4){
+//     console.log("Condition is true")
+// }else{
+//     console.log("Condition is flase")
+// }
+var choice = 21;
+switch (choice) {
+    case 1:
+        console.log("U r in case 1");
+        break;
+    case 2:
+        console.log("U r in case 2");
+        break;
+    default:
+        console.log("U r in default case");
+        break;
+}
