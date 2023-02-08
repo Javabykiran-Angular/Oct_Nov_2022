c3cf0c906da61ca26da8fb97df0ea6b7060ea03a
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-02-08 16:19:25 +0530

    Day11

---
+++ Typescript/Day11/interfaceMain.ts
@@ -0,0 +1,5 @@
+import {EmployeeDetails} from './employeeDetails';
+
+let EmpObj=new EmployeeDetails('Sumit','Raokhande',100000,'DevOps');
+EmpObj.display();
+
---
+++ Typescript/Day11/mainInheritance.ts
@@ -0,0 +1,12 @@
+
+import {Circle} from './circle';
+import {Rectangle} from './rectangle';
+
+    let circleObj=new Circle(2);
+    circleObj.myArea();
+    circleObj.display();
+
+
+    let RectObj=new Rectangle(2,3);
+    RectObj.myArea();
+    RectObj.display();
