215673c73c012e62304a67ac33edf217ea89c94c
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-02-11 16:10:19 +0530

    Day03

---
+++ Angular/Day03/propertybinding/propertybinding.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-propertybinding',
+  templateUrl: './propertybinding.component.html',
+  styleUrls: ['./propertybinding.component.css']
+})
+export class PropertybindingComponent implements OnInit {
+
+  name:string='Sumit';
+  imgUrl:string='../../assets/bg4.jpg';
+  mycolSpan:number=2;
+  isHidden:boolean=false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
