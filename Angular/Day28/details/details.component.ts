eeffaafdfe5d13cb32095be360cb2cc01760a8e0
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-03-31 15:59:42 +0530

    Day28

---
+++ Angular/Day28/details/details.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Employee } from '../model/employee';
+import { HttpService } from '../utility/http.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+
+  empobj=<Employee>{};
+
+  constructor(private route:ActivatedRoute,
+              private service:HttpService) { }
+
+  ngOnInit(): void {
+    this.GetdataFromUrl();
+  }
+
+  GetdataFromUrl(){
+    this.route.paramMap
+    .subscribe((param:any)=>{
+      console.log(param.get('id'))
+      this.GetParticularData(param.get('id'))
+    })
+  }
+
+  GetParticularData(id:any){
+    this.service.getParticularRecord(id)
+    .subscribe((response:any)=>{
+        // console.log(response);
+        this.empobj=response;
+        console.log(this.empobj);
+
+    });
+  }
+
+  OnUpdate(){
+    
+  }
+
+}
